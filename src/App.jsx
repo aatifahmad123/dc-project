3223e4c5c1253a8675293b80b746c96678044c0c
Aatif Ahmad <b22ai002@iitj.ac.in>
2024-10-23 19:24:05 +0530

    commit one

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div style={{ fontSize: "32px" }}>
+      DC Project - 5th semester under Venkat Ram Reddy
+    </div>
+  );
+}
+
+export default App;
